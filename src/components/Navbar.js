604a8230bf51d57ad48ec1e4f7e779eb83540210
anki009 <ankum1999@gmail.com>
2023-01-28 01:12:34 +0530

    added files

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import logo from "../assets/react.svg";
+
+export default function Navbar(props) {
+  // return (
+  //   <div>
+  //     <nav>
+  //     <img src={logo} alt="" className="nav-icon" />
+  //           <h3 className="nav-logo_text">ReactFacts</h3>
+  //           <h4 className="nav-title">React Course - Project 1</h4>
+  //     </nav>
+  //   </div>
+  // );
+  return (
+    <nav className={props.darkMode ? "dark" : ""}>
+      <img src={logo} alt="" className="nav-icon" />
+      <h3 className="nav-logo_text">ReactFacts</h3>
+
+      <div className="toggler">
+        <p className="toggler-light">Light</p>
+        <div className="toggler-slider" onClick={props.toggleDarkMode}>
+          <div className="toggler-slider-circle"></div>
+        </div>
+        <p className="toggler-dark">Dark</p>
+      </div>
+    </nav>
+  );
+}
